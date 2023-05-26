420cd92a0998c0dd9ef7e43cc972b2dc4eaf4c01
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2023-05-26 12:51:26 +0200

    almost all

--- processing/utils.py
+++ processing/utils.py
@@ -1,30 +1,30 @@
 import numpy as np
 import cv2 as cv
 
-defTrackbarMin = 60
-defTrackbarMax = 140
+defTrackbarMin = 30
+defTrackbarMax = 65
 
 
 def getContrast(
     image: np.ndarray, topHatSize: int, blackHatSize: int, dilateSize: int
 ) -> np.ndarray:
-    kernelTop = cv.getStructuringElement(cv.MORPH_ELLIPSE, (topHatSize, topHatSize))
-    kernelBlack = cv.getStructuringElement(
-        cv.MORPH_ELLIPSE, (blackHatSize, blackHatSize)
-    )
-    tophat = cv.morphologyEx(image, cv.MORPH_TOPHAT, kernelTop)
-    blackhat = cv.morphologyEx(image, cv.MORPH_BLACKHAT, kernelBlack)
+    # kernelTop = cv.getStructuringElement(cv.MORPH_ELLIPSE, (topHatSize, topHatSize))
+    # kernelBlack = cv.getStructuringElement(
+    #     cv.MORPH_ELLIPSE, (blackHatSize, blackHatSize)
+    # )
+    # tophat = cv.morphologyEx(image, cv.MORPH_TOPHAT, kernelTop)
+    # blackhat = cv.morphologyEx(image, cv.MORPH_BLACKHAT, kernelBlack)
 
-    addition = cv.add(tophat, image)
-    subtraction = cv.subtract(addition, blackhat)
+    # addition = cv.add(tophat, image)
+    # subtraction = cv.subtract(addition, blackhat)
 
-    alpha = 1.5  # Contrast control (1.0-3.0)
-    beta = 10  # Brightness control (0-100)
+    alpha = 1.1  # Contrast control (1.0-3.0)
+    beta = 1  # Brightness control (0-100)
 
-    adjusted = cv.convertScaleAbs(subtraction, alpha=alpha, beta=beta)
+    adjusted = cv.convertScaleAbs(image, alpha=alpha, beta=beta)
     # kerneldilate = cv.getStructuringElement(cv.MORPH_RECT, (dilateSize, dilateSize))
     # result = cv.dilate(subtraction, kerneldilate)
-
+    # cv.imshow("subtraction", cv.resize(adjusted, (800, 600)))
     return adjusted
 
 
@@ -33,12 +33,15 @@ def getPlate(image: np.ndarray, min: int, max: int):
     canny = cv.Canny(image, min, max)
 
     dilation_size = 5
+    # (2 * dilation_size + 1, 2 * dilation_size + 1),
+    #     (dilation_size, dilation_size),
     element = cv.getStructuringElement(
         cv.MORPH_RECT,
-        (2 * dilation_size + 1, 2 * dilation_size + 1),
         (dilation_size, dilation_size),
+        # (dilation_size, dilation_size),
     )
-    dilated = cv.dilate(canny, element)
+    # np.ones((7, 7), np.uint8)
+    dilated = cv.dilate(canny, element, iterations=1)
 
     contours, hierarchy = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     plateContour = []
@@ -49,7 +52,7 @@ def getPlate(image: np.ndarray, min: int, max: int):
             x, y, w, h = cv.boundingRect(contour)
             ratio = float(h / w)
             area = cv.contourArea(contour)
-            if ratio >= 0.15 and ratio <= 0.6 and area >= 90000:
+            if ratio >= 0.15 and ratio <= 0.65 and area >= 90000:
                 plateContour.append(contour)
     return dilated, plateContour, contours
 
@@ -64,15 +67,11 @@ def perform_processing(image: np.ndarray) -> str:
     cv.namedWindow(winName)
     cv.createTrackbar("min", winName, defTrackbarMin, 255, onTrack)
     cv.createTrackbar("max", winName, defTrackbarMax, 255, onTrack)
+
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-    bilateralSize = 12
-    blurred = cv.bilateralFilter(gray, bilateralSize, 17, 17)
-
-    contrasted = getContrast(blurred, 8, 8, 7)
-
-    blurSize = 7
-    blurred = cv.GaussianBlur(contrasted, (blurSize, blurSize), 0)
+    gray = getContrast(gray, 3, 3, 0)
+    blurred = cv.GaussianBlur(gray, (7, 7), 0)
+    blurred = cv.morphologyEx(blurred, cv.MORPH_OPEN, np.ones((5, 5), np.uint8))
 
     while True:
         min = cv.getTrackbarPos("min", winName)
@@ -80,23 +79,19 @@ def perform_processing(image: np.ndarray) -> str:
         defTrackbarMax = max
         defTrackbarMin = min
         canny, contour, contours = getPlate(blurred, min, max)
-        cv.drawContours(image, contours, -1, (255, 0, 0), 3)
         if len(contour) != 0:
             cv.drawContours(image, contour, -1, (0, 0, 255), 10)
 
-        contrasted = cv.resize(contrasted, (800, 600))
         canny = cv.resize(canny, (800, 600))
         image = cv.resize(image, (800, 600))
-        # cv.imshow("contrasted", contrasted)
-        # cv.imshow("image", image)
-        # cv.imshow("canny", canny)
 
         grey_3_channel = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
 
         numpy_horizontal = np.hstack((image, grey_3_channel))
 
         numpy_horizontal_concat = np.concatenate((image, grey_3_channel), axis=1)
-        cv.imshow("result", numpy_horizontal_concat)
+        cv.imshow(winName, numpy_horizontal_concat)
+        # cv.imshow("gray", cv.resize(gray, (800, 600)))
 
         if cv.waitKey(30) == ord("q"):
             cv.destroyAllWindows()
